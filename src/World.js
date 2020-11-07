f8912a32f90d139f13358195c3dfda170529495d
Joni Korpi <me@jonikorpi.com>
2020-11-07 23:20:50 +0200

    Add scroll-orbit camera

--- src/World.js
+++ src/World.js
@@ -5,22 +5,18 @@ import { useCamera } from "./Camera.js";
 import { useRenderer, useLoop } from "./WebGL.js";
 import { pointOnCircle } from "./helpers/maths.js";
 import { hexesInRadius } from "./helpers/hexes.js";
+import { useScroller } from "./helpers/useScroller.js";
 
 const cameraFocus = [0, 0, 0.618];
 const cameraDistance = 15;
 const cameraHeight = 35;
-const cameraAngle = 0;
+let cameraAngle = 0;
 
 const World = () => {
   const { updateAllUniforms, setClear, clear } = useRenderer();
   const camera = useCamera();
-  setClear([50 / 255, 32 / 255, 62 / 255, 1]);
 
-  useEffect(() => {
-    updateAllUniforms("light", [1, 0, -1]);
-  });
-
-  useLoop((timestamp, clock, frameNumber) => {
+  const handleCamera = () => {
     pointOnCircle(cameraDistance, cameraAngle, camera.position);
     camera.position[0] += cameraFocus[0];
     camera.position[1] += cameraFocus[1];
@@ -33,15 +29,31 @@ const World = () => {
     //   normalizeInPlace(camera.direction);
     camera.update();
 
-    updateAllUniforms("time", clock / 1000);
     updateAllUniforms("projectionView", camera.projView);
     updateAllUniforms("cameraPosition", camera.position);
+  };
+
+  const handleScroll = ({ distanceChange }) => {
+    cameraAngle -= (distanceChange / 500) % (Math.PI * 2);
+    handleCamera();
+  };
+  useScroller(undefined, handleScroll, true);
+
+  useEffect(() => {
+    handleCamera();
+    setClear([50 / 255, 32 / 255, 62 / 255, 1]);
+    updateAllUniforms("light", [1, 0, -1]);
+  });
+
+  useLoop((timestamp, clock, frameNumber) => {
+    updateAllUniforms("time", clock / 1000);
     clear();
   });
 
   return (
     <>
-      <h1>POHOJOLA</h1>
+      <div style={{ height: "1000vmax" }} />
+
       {hexesInRadius(16).map((hex) => (
         <Ground key={hex} coordinates={hex} />
       ))}
